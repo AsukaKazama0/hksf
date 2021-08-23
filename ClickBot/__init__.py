0c8d1e7d6cac635874dbf30c48caeb7c9b2fff2b
AsukaKazama0 <80569912+AsukaKazama0@users.noreply.github.com>
2021-08-23 07:22:09 +0530

    Update __init__.py

--- ClickBot/__init__.py
+++ ClickBot/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-STRING_HASH = config("BOT_TOKEN", default=None)
+STRING_HASH = config("SESSION_STRING", default=None)
 APP_ID = 7577827
 
 Bot = TelegramClient('Bot', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
